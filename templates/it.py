441c1630ca24e88735f9ba772fc44be54719e7cd
Luca Postregna <luca.postregna@gmail.com>
2011-07-21 09:44:52 +0200

    fix description

--- templates/it.py
+++ templates/it.py
@@ -2,7 +2,7 @@ import datetime
 
 language = "it"
 keywords = "taverna, ristorante, ristorazione, cividale, friuli, paradiso, bassanese, valli del natisone"
-description = "Ristorante Taverna Paradiso alla Bassanese di Cividale del Friuli"
+description = "Ristorante a Cividale del Friuli"
 site_name = "Taverna Paradiso"
 subname = "(alla Bassanese)"
 site_desc = "Ristorante a Cividale del Friuli"
