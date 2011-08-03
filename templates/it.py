6d22e6899e322e98a6d3cc6cb015201dd3994474
Luca Postregna <luca.postregna@gmail.com>
2011-08-03 19:28:51 +0200

    fix

--- templates/it.py
+++ templates/it.py
@@ -1,7 +1,7 @@
 import datetime
 
 language = "it"
-keywords = "ristorante a cividale del friuli, taverna, trattoria, ristorazione, mangiare, ristorazione"
+keywords = "ristorante a cividale, taverna, trattoria, mangiare, ristorazione, friuli"
 description = "Ristorante a Cividale del Friuli, Trattoria"
 title = "Taverna Paradiso (alla Bassanese) - Ristorante a Cividale del Friuli"
 site_name = "Taverna Paradiso"
